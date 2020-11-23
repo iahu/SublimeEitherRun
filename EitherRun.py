6e0cacbfffa6028a0cf9ec5de0c4055e0eb0526c
i18u <iahu1988@gmail.com>
2020-11-23 10:26:38 +0800

    fix: support v2

--- EitherRun.py
+++ EitherRun.py
@@ -3,22 +3,26 @@ import sublime_plugin
 
 from subprocess import PIPE
 from subprocess import Popen
+from sys import version_info
 
-from .utils import is_windows, get_proc_env
+if version_info[0] == 2:
+  from utils import is_windows, get_proc_env
+else:
+  from .utils import is_windows, get_proc_env
 
 class EitherRunCommand(sublime_plugin.TextCommand):
-	def run(self, edit, cmd, resolve = None, reject = None):
-		command = cmd.split(' ') if isinstance(cmd, str) else cmd
-		proc = Popen(
-		    command, stdin=PIPE,
-		    stderr=PIPE,
-		    stdout=PIPE,
-		    env=get_proc_env(),
-		    shell=is_windows())
+  def run(self, edit, cmd, resolve = None, reject = None):
+    command = cmd.split(' ') if isinstance(cmd, str) else cmd
+    proc = Popen(
+        command, stdin=PIPE,
+        stderr=PIPE,
+        stdout=PIPE,
+        env=get_proc_env(),
+        shell=is_windows())
 
-		stdout, stderr = proc.communicate()
+    stdout, stderr = proc.communicate()
 
-		if proc.returncode == 0 and resolve:
-			self.view.run_command(resolve)
-		elif reject:
-			self.view.run_command(reject)
+    if proc.returncode == 0 and resolve:
+      self.view.run_command(resolve)
+    elif reject:
+      self.view.run_command(reject)
